e0d0b698245a1789c5762a7a513eace7631280c4
Nick Mathews <nickel3956@gmail.com>
2023-11-20 17:18:21 -0500

    Bump version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "dev.mathewsmobile.runes"
         minSdk = 27
         targetSdk = 34
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
