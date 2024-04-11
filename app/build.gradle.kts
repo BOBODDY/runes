b45761aba3d0bf2b67fe8a17c0732020414d1d60
Nick Mathews <nickel3956@gmail.com>
2024-04-10 22:35:25 -0400

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "dev.mathewsmobile.runes"
         minSdk = 27
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
